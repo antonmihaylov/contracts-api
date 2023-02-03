ecdb150aca9b292d821d0b4dc817d9756319e8b9
Anton Mihaylov <antonmmihaylov@gmail.com>
2023-02-03 13:39:20 +0200

    fix: balances api

--- src/controllers/balances.ts
+++ src/controllers/balances.ts
@@ -9,7 +9,7 @@ const balancesRouter = express.Router()
 /**
  * @returns all unpaid and in-progress jobs for a user (either as a contractor or a client)
  */
-balancesRouter.get('/deposit/:userId', getProfile, async (req, res) => {
+balancesRouter.post('/deposit/:userId', getProfile, async (req, res) => {
   const { profileService } = req
   const userId = parseInt(req.params.userId, 10)
   const { amount } = req.body as { amount: number }
